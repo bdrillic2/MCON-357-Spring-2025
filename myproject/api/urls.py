c93c3b82e86787e8830f811a65b07ae4e46463ba
bdrillic2 <bdrillic2@student.touro.edu>
2025-04-08 18:37:48 -0400

    completed Django project setup

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import user_list
+from django.contrib import admin
 
 urlpatterns = [
     path('users/', user_list, name='user-list'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -90,16 +90,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # }
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": getenv("DB_NAME"),
-        "USER": getenv("DB_USER"),
-        "PASSWORD": getenv("DB_PASSWORD"),
-        "HOST": getenv("DB_HOST"),
-        "PORT": getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',   # database will be created automatically
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
